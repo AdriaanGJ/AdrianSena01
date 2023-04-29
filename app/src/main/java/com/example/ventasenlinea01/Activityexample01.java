84e1f2a95f4313dffeba4075d4c1fcf894b5fd61
Adrian  Garcia <garciadrianedu@gmail.com>
2023-04-28 21:29:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ventasenlinea01/Activityexample01.java
@@ -0,0 +1,14 @@
+package com.example.ventasenlinea01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activityexample01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityexample01);
+    }
+}
